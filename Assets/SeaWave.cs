73c2ed4c46fac699294dee162aff3d50d462bfce
kksumm123 <kksumm123@gmail.com>
2021-06-13 21:19:35 +0900

    파도 Y증가속도 제한 추가

--- Assets/SeaWave.cs
+++ Assets/SeaWave.cs
@@ -6,11 +6,13 @@ public class SeaWave : MonoBehaviour
 {
     [SerializeField] float speed = 0.5f;
     [SerializeField] float speedAcceleration = 0.1f;
+    [SerializeField] float speedLimit = 4.5f;
     void Update()
     {
         var pos = transform.position;
         pos.y += speed * Time.deltaTime;
         transform.position = pos;
         speed += speedAcceleration * Time.deltaTime;
+        speed = Mathf.Min(speed, speedLimit);
     }
 }
