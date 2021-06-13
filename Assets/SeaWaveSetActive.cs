67fbc2765e2ff8d7d6e753c6265b0b5c755c0f74
kksumm123 <kksumm123@gmail.com>
2021-06-13 21:25:08 +0900

    SeaWavePosition2 딜레이 추가

---
+++ Assets/SeaWaveSetActive.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaWaveSetActive : MonoBehaviour
+{
+    [SerializeField] Transform targetTr;
+    [SerializeField] float activeDelay = 0.3f;
+    IEnumerator Start()
+    {
+        targetTr = transform.Find("SeaWavePosition2");
+
+        targetTr.gameObject.SetActive(false);
+        yield return new WaitForSeconds(activeDelay);
+        targetTr.gameObject.SetActive(true);
+    }
+
+}
